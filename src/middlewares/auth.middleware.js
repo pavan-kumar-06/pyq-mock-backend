48ece6a265ae430addc08914db8c511848c93b2e
pavan-kumar-06 <nandamuripavan87@gmail.com>
2024-04-26 14:51:50 +0530

    made changes to models

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,29 +2,36 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 import { Admin } from "../models/admin.models.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-export const verifyJWT = asyncHandler(async(req, _, next) => {
+export const verifyJWT = asyncHandler(async(req, res, next) => {
     try {
         // console.log(req.cookies)
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
         
         // console.log(token);
         if (!token) {
-            throw new ApiError(401, "Unauthorized request")
-        }
+            return res
+            .json(new ApiResponse(407,  "Invalid Access Token"))
+            }
     
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
         const admin = await Admin.findById(decodedToken?._id).select("-password -refreshToken")
     
         if (!admin) {
-            throw new ApiError(401, "Invalid Access Token")
-        }
+            return res
+            .json(new ApiResponse(407,  "Invalid Access Token"))
+            }
     
         req.admin = admin;
         next()
     } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid access token")
+        console.log("Invalid Token")
+        return res
+        .json(new ApiResponse(407,  "Invalid Access Token"))
+
+        // throw new ApiError(402, error?.message || "Invalid access token")
     }
     
 })
--- src/models/attempt.models.js
+++ src/models/attempt.models.js
@@ -1,60 +1,67 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const responseSchema = new Schema({
-    answerSubmitted:{
-        type: Number,
-        required: true
-    },
-    subject: {
-        type: String,
-        enum: ["physics", "chemistry", "biology", "maths"], // Add more subjects if needed
-        required: true
-    },
-    timeTaken: {
-        type: Number, // in seconds
-        default: 0,
-        required: true
-    }
-});
+// const responseSchema = new Schema({
+//     // answerSubmitted:{
+//     //     type: Number,
+//     //     required: true
+//     // },
+//     // subject: {
+//     //     type: String,
+//     //     enum: ["physics", "chemistry", "biology", "maths"], // Add more subjects if needed
+//     //     required: true
+//     // },
+//     // isCorrect:{
+//     //     type: Boolean,
+//     //     default: false
+//     // }
+//     // timeTaken: {
+//     //     type: Number, // in seconds
+//     //     default: 0,
+//     //     required: true
+//     // }
+// });
 
-
-const attemptSchema = new Schema({
+const attemptSchema = new Schema(
+  {
     userId: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     testId: {
-        type: Schema.Types.ObjectId,
-        ref: "Test",
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "Test",
+      required: true,
     },
-    attemptCount: {
-        type: Number,
-        default: 0,
-        validate: {
-          validator: function(v) {
-            // Ensure that the attemptCount does not exceed 1
-            return v >= 0 && v <= 2;
-          },
-          message: props =>
-            `${props.value} Maximum attempt allowed is 2.`
-        }
-    },
-    startTime: {
-        type: Date,
-        default: Date.now // Default value is the current timestamp when the document is created
-    },
+    // attemptCount: {
+    //     type: Number,
+    //     default: 0,
+    //     validate: {
+    //       validator: function(v) {
+    //         // Ensure that the attemptCount does not exceed 1
+    //         return v >= 0 && v <= 2;
+    //       },
+    //       message: props =>
+    //         `${props.value} Maximum attempt allowed is 2.`
+    //     }
+    // },
+    // startTime: {
+    //     type: Date,
+    //     default: Date.now // Default value is the current timestamp when the document is created
+    // },
     score: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
-    response: [
-        responseSchema
-    ]
-}, {timestamps: true})
+    responses: {
+      type: [Number],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
 
-attemptSchema.plugin(mongooseAggregatePaginate)
+attemptSchema.plugin(mongooseAggregatePaginate);
 
 export const Attempt = mongoose.model("Attempt", attemptSchema);
